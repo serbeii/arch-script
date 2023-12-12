9df6d5d86dbe7c2484e3afa3f4e40b9fb512ced0
serbeii <cem.serimer@tedu.edu.tr>
2023-12-13 00:31:40 +0300

    fixed the append logic for packages

--- systemconf.lua
+++ systemconf.lua
@@ -81,22 +81,22 @@ local packages = io.open("pkglist.txt","a+")
 -- Install the correct microcode and vulkan drivers
 local cpu_vendor = io.popen("lshw -C cpu | grep vendor"):read("*a")
 if cpu_vendor:lower():match("intel") then
-   packages:write("intel-ucode")
+   packages:write("intel-ucode\n")
 elseif cpu_vendor:lower():match("amd") then
-   packages:write("amd-ucode")
+   packages:write("amd-ucode\n")
 end
 
 local gpu_vendor = io.popen("lshw -C display | grep vendor"):read("*a")
 if gpu_vendor:lower():match("intel") then
-    packages:write("vulkan-intel")
-    packages:write("lib32-vulkan-intel")
+    packages:write("vulkan-intel\n")
+    packages:write("lib32-vulkan-intel\n")
 elseif gpu_vendor:lower():match("amd") then
-    packages:write("vulkan-radeon")
-    packages:write("lib32-vulkan-radeon")
+    packages:write("vulkan-radeon\n")
+    packages:write("lib32-vulkan-radeon\n")
 end
 
-if (io.popen("lshw | grep battery"):read("*a")) then
-    packages:write("tlp")
+if (io.popen("lshw | grep battery"):read("*a"):lower():match("battery")) then
+    packages:write("tlp\n")
 end
 packages:close()
 os.execute("pacman -Syu - < pkglist.txt")
