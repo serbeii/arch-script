41748248897e2b88cece03cc9a3b432843ee94ac
serbeii <cem.serimer@tedu.edu.tr>
2024-02-29 20:46:21 +0300

    added nvidia settings and updated package list

--- systemconf.lua
+++ systemconf.lua
@@ -24,8 +24,8 @@ os.execute("hwclock --systohc")
 os.execute("locale-gen")
 local hostname = "/etc/hostname"
 
-local host = io.open(hostname,"a")
-host:write("serbeii","\n")
+local host = io.open(hostname, "a")
+host:write("serbeii", "\n")
 host:close()
 
 -- Creation of the user
@@ -72,12 +72,12 @@ end
 os.execute("pacman -Syu " .. table.concat(initialPackages, " "))
 
 -- Install the correct microcode and vulkan drivers
-local packages = io.open("pkglist.txt","a+")
+local packages = io.open("pkglist.txt", "a+")
 local cpu_vendor = io.popen("lshw -C cpu | grep vendor"):read("*a")
 if cpu_vendor:lower():match("intel") then
-   packages:write("intel-ucode\n")
+    packages:write("intel-ucode\n")
 elseif cpu_vendor:lower():match("amd") then
-   packages:write("amd-ucode\n")
+    packages:write("amd-ucode\n")
 end
 
 local gpu_vendor = io.popen("lshw -C display | grep vendor"):read("*a")
@@ -87,6 +87,9 @@ if gpu_vendor:lower():match("intel") then
 elseif gpu_vendor:lower():match("amd") then
     packages:write("vulkan-radeon\n")
     packages:write("lib32-vulkan-radeon\n")
+elseif gpu_vendor:lower():match("nvidia") then
+    packages:write("nvidia\n")
+    packages:write("lib32-nvidia-utils\n")
 end
 
 if (io.popen("lshw | grep battery"):read("*a"):lower():match("battery")) then
@@ -105,38 +108,38 @@ local eduroam = {
     "",
     "[Security]",
     "EAP-Method=PEAP",
-    "EAP-Identity="..mail,
+    "EAP-Identity=" .. mail,
     "#Password not to be saved, will be queried through the agent",
     "EAP-PEAP-Phase2-Method=MSCHAPV2",
-    "EAP-PEAP-Phase2-Identity="..mail,
+    "EAP-PEAP-Phase2-Identity=" .. mail,
 }
 
 local file, err = io.open("/var/lib/iwd/eduroam.8021x", "w")
-    if not file then
-        error("Error opening file: " .. err)
-    end
+if not file then
+    error("Error opening file: " .. err)
+end
 
-    for _, line in ipairs(eduroam) do
-        file:write(line .. "\n")
-    end
+for _, line in ipairs(eduroam) do
+    file:write(line .. "\n")
+end
 
 file:close()
 
 -- Install config files for nvim and hyprland from specific git repositories
-os.execute("mkdir -p /home/"..username.."/.config")
-os.execute("cd /home/"..username.." && git clone https://github.com/serbeii/dotfiles.git")
-os.execute("cd /home/"..username.."/.config && git clone https://github.com/serbeii/nvim.git")
+os.execute("mkdir -p /home/" .. username .. "/.config")
+os.execute("cd /home/" .. username .. " && git clone https://github.com/serbeii/dotfiles.git")
+os.execute("cd /home/" .. username .. "/.config && git clone https://github.com/serbeii/nvim.git")
 
 require("link")
 
-Link.linkFolders("/home/"..username)
+Link.linkFolders("/home/" .. username)
 
 if not os.execute("chown -R " .. username .. " /home/" .. username) then
     print("please execute chown -R " .. username .. " /home/" .. username)
 end
 os.execute("visudo")
-if not os.execute("chown -R ".. username .. " /arch-script") then
-    print ("please execute chown -R " .. username .. " /arch-script")
+if not os.execute("chown -R " .. username .. " /arch-script") then
+    print("please execute chown -R " .. username .. " /arch-script")
 end
 
 --GRUB setup
